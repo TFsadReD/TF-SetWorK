1572d02accd21987ca93be822c9670bdce176344
TFsadReD <MaiK.HeliO@yandex.ru>
2025-09-08 22:18:22 +0300

    0.5.8 - Finally, bool's file type is fully implemented

--- core/interpreter.py
+++ core/interpreter.py
@@ -1,76 +1,84 @@
 from core.lexer import lexicon, Token
 
+
+
 class Interpreter:
     def __init__(self):
         self.variables = {}
 
+
     """
     Нормализация чисел
     """
+
+
     def _normalize_float(self, value):
         if isinstance(value, float):
             value = round(value, 10)
             value = float(f"{value:.10g}")
         return value
 
+
     """
     Обработка токенов
     """
+
+
     def eval_expr(self, tokens, line_num, line_text, postfix=False):
 
         if not tokens:
             return ""
 
-        def apply_op(a, op, b):
+        def apply_op(a, op, b=None):
             match op:
                 case '+':
                     if isinstance(a, (int,float)) and isinstance(b,(int,float)):
                         return self._normalize_float(a + b)
-                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value")
+                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value\n > {line_text}")
                 case ',+':
                     if isinstance(a, (int,float)) and isinstance(b,(int,float)):
                         return self._normalize_float(float(a) + float(b))
-                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value")
+                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value\n > {line_text}")
                 case ',-':
                     if isinstance(a, (int,float)) and isinstance(b,(int,float)):
                         return self._normalize_float(float(a) - float(b))
-                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value")
+                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value\n > {line_text}")
                 case '$+':
                     return str(a) + str(b)
                 case '-':
                     if isinstance(a, (int,float)) and isinstance(b,(int,float)):
                         return self._normalize_float(a - b)
-                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value")
+                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value\n > {line_text}")
                 case '*':
                     if isinstance(a, (int,float)) and isinstance(b,(int,float)):
                         return self._normalize_float(a * b)
-                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value")
+                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value\n > {line_text}")
                 case '/':
                     if isinstance(a, (int,float)) and isinstance(b,(int,float)):
                         return self._normalize_float(a / b)
-                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value")
-                # логические и сравнения
-                case '==':
-                    return a == b
-                case '!=':
-                    return a != b
-                case '<':
-                    return a < b
-                case '<=':
-                    return a <= b
-                case '>':
-                    return a > b
-                case '>=':
-                    return a >= b
+                    raise TypeError(f"TypeError in Line {line_num}: Invalid operator's value\n > {line_text}")
+
+                case '==': return a == b
+                case '!=': return a != b
+                case '<':  return a < b
+                case '<=': return a <= b
+                case '>':  return a > b
+                case '>=': return a >= b
+
+                case 'and': return bool(a) and bool(b)
+                case 'or':  return bool(a) or bool(b)
+                case 'not': return not bool(a)
+
                 case _:
-                    raise SyntaxError(f"Unknown operator {op}")
+                    raise SyntaxError(f"SyntaxError in Line {line_num}: Unknown operator {op}\n > {line_text}")
 
         precedence = {
             '+': 1, '-': 1,
             ',+': 2, ',-': 2,
             '*': 3, '/': 3,
             '==': 0, '!=': 0, '<': 0, '<=': 0, '>': 0, '>=': 0,
-            '$+': 4
+            'and': -1, 'or': -2, 'not': 4,
+            '$+': 5
         }
 
         output, ops = [], []
@@ -80,10 +88,14 @@ class Interpreter:
                 case "NUMBER" | "STRING" | "BOOL":
                     output.append(tok.value)
                 case "ID":
-                    if tok.value in self.variables:
+                    if tok.value in ("and", "or", "not"):
+                        while ops and ops[-1] != '(' and precedence.get(ops[-1], 0) >= precedence.get(tok.value, 0):
+                            output.append(ops.pop())
+                        ops.append(tok.value)
+                    elif tok.value in self.variables:
                         output.append(self.variables[tok.value])
                     else:
-                        raise NameError(f"NameError in Line {line_num}: Undefined variable '{tok.value}'\n> {line_text}")
+                        raise NameError(f"NameError in Line {line_num}: Undefined variable '{tok.value}'\n > {line_text}")
                 case "OPER":
                     while ops and ops[-1] != '(' and precedence.get(ops[-1], 0) >= precedence.get(tok.value, 0):
                         output.append(ops.pop())
@@ -94,44 +106,52 @@ class Interpreter:
                     while ops and ops[-1] != '(':
                         output.append(ops.pop())
                     if not ops or ops[-1] != '(':
-                        raise SyntaxError(f"SyntaxError in Line {line_num}: Mismatched parentheses\n> {line_text}")
+                        raise SyntaxError(f"SyntaxError in Line {line_num}: Mismatched parentheses\n > {line_text}")
                     ops.pop()
                 case _:
-                    raise SyntaxError(f"SyntaxError in Line {line_num}: Invalid token {tok}\n> {line_text}")
+                    raise SyntaxError(f"SyntaxError in Line {line_num}: Invalid token\n > {line_text}")
 
         while ops:
             if ops[-1] in ('(', ')'):
-                raise SyntaxError(f"SyntaxError in Line {line_num}: Mismatched parentheses\n> {line_text}")
+                raise SyntaxError(f"SyntaxError in Line {line_num}: Mismatched parentheses\n > {line_text}")
             output.append(ops.pop())
 
         if postfix:
             print("Postfix stack:", output)
 
         stack = []
+        operators_check = (',-',',+','$+','==','!=','<','<=','>','>=','and','or','not')
         for item in output:
-            if isinstance(item, (int, float, bool)) or (isinstance(item, str) and item not in (',-',',+','$+','==','!=','<','<=','>','>=')):
+            if isinstance(item, (int, float, bool)) or (isinstance(item, str) and item not in operators_check):
                 stack.append(item)
             else:
                 try:
-                    b = stack.pop()
-                    a = stack.pop()
-                    stack.append(apply_op(a, item, b))
+                    if item == 'not':
+                        a = stack.pop()
+                        stack.append(apply_op(a, item))
+                    else:
+                        b = stack.pop()
+                        a = stack.pop()
+                        stack.append(apply_op(a, item, b))
                 except Exception:
                     raise SyntaxError(f"SyntaxError in Line {line_num}: Invalid expression\n > {line_text}")
 
         if len(stack) != 1:
-            raise SyntaxError(f"StackError in Line {line_num}: Invalid stack expression\n> {line_text}")
+            raise SyntaxError(f"StackError in Line {line_num}: Invalid stack expression\n > {line_text}")
 
         result = stack[0]
         return self._normalize_float(result)
 
+
     """
-    Запуск интерпритатора
+    Запуск интерпретатора
     """
+
+
     def run(self, code, postfix=False):
         lines = code.strip().splitlines()
         if not lines or not lines[0].startswith("!TF:"):
-            raise SyntaxError("File must start with !TF:")
+            raise SyntaxError("CriticalError: There is no guide determinant for the entire code")
 
         for line_num, line in enumerate(lines[1:], start=2):
             if not line.strip():
@@ -149,11 +169,12 @@ class Interpreter:
                     case "ID" if len(tokens) >= 3 and tokens[1].value == "@=":
                         var_name = tokens[0].value
                         expr_tokens = tokens[2:]
-                        self.variables[var_name] = self._normalize_float(
-                            self.eval_expr(expr_tokens, line_num, line)
-                        )
+                        value = self.eval_expr(expr_tokens, line_num, line)
+                        if isinstance(value, (int, float)):
+                            value = self._normalize_float(value)
+                        self.variables[var_name] = value
                     case _:
-                        raise SyntaxError(f"Line {line_num}: Unknown statement\n> {line}")
+                        raise SyntaxError(f"Line {line_num}: Unknown statement\n > {line}")
             except Exception as e:
                 print(f"~ ~ ~ ~ ~ ~ ~ ~\n{e}")
                 break
