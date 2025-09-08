e190af2b6f18ef9a93390582dcc0db106d9a1279
TFsadReD <MaiK.HeliO@yandex.ru>
2025-09-08 22:06:01 +0300

    ReadMe changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from core.interpreter import Interpreter
 
-version = ">>> TF SetWorK v.0.5.2 <<<"
+version = ">>> TF SetWorK v.0.5.3 <<<"
 
 if __name__ == "__main__":
     with open("app.tf", "r", encoding="utf-8") as f:
